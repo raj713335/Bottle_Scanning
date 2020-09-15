2c229c66b3e513549af828135d7d311b46806e40
ARNAB DAS <raj713335@gmail.com>
2020-09-15 11:32:02 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import tkinter as tk
+import tkinter.ttk as ttk
+from datetime import date, datetime
+from threading import Thread
+import time
+import socket
+import subprocess
+import calendar
+
+
+global version
+version="1.0.0"
+
+
