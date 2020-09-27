37b75bc4c7a4a82c60f50c083a6fb9ad5bd59010
ARNAB DAS <raj713335@gmail.com>
2020-09-28 00:18:21 +0530

    Final

--- master.py
+++ master.py
@@ -7,8 +7,6 @@ from PIL import Image, ImageTk
 import cv2
 
 
-global version
-version = "1.0.0"
 
 
 
@@ -74,8 +72,8 @@ def main():
         window_user_login.attributes('-alpha', 0.97)
 
         user_login_window = User_Login(window_user_login)
-        window_user_login.iconbitmap(default='DATA/IMAGES/icons/favicon.ico')
-        window_user_login.title('Admin Login ' + version)
+        #window_user_login.iconbitmap(default='DATA/IMAGES/icons/favicon.ico')
+        window_user_login.title('Admin Login ' + '1')
         window_user_login.geometry("600x450")
         window_user_login.mainloop()
 
@@ -90,6 +88,8 @@ def main():
 
 
             def __init__(self, window):
+
+                self.windows=window
 
                 with open('DATA/Scanning/data.txt', 'r') as fh:
                     all_lines = fh.readlines()
@@ -150,7 +150,6 @@ def main():
                     serial=e1
 
 
-
                 def turn_button(x=0):
 
                     self.txtfld1.destroy()
@@ -205,16 +204,10 @@ def main():
                 self.txtfld4.place(x=270, y=310, width=260)
                 self.txtfld4.insert(0, total)
 
+
                 self.lb5 = tk.Label(window, text="Batch Size", font=("Helvetica", 10), bg='#EFEFEF')
-                #self.lb5.place(x=60, y=330)
-
-                self.txtfld5 = ttk.Entry(window,
-                                         font=("Helvetica", 10))
-                #self.txtfld5.place(x=270, y=330, width=260)
+                self.txtfld5 = ttk.Entry(window,font=("Helvetica", 10))
                 self.txtfld5.insert(0, serial)
-
-                # self.btn_back = ttk.Button(window, text="BACK", width=20, command=self.validate)
-                # self.btn_back.place(x=60, y=380, width=130, height=40)
 
 
                 self.btn_quit = ttk.Button(window, text="RESET", width=20, command=self.reset)
@@ -293,16 +286,9 @@ def main():
 
                 try:
 
-                    #print(1)
                     temp=int((self.txtfld4.get()))
 
                     d1=str(self.txtfld4.get())
-
-
-
-                    #print(3)
-
-
 
 
                 except:
@@ -315,7 +301,7 @@ def main():
 
 
 
-                # IF VALIDATION IS SUCCESFULL THEN IT OPENS USER EDIT WINDOW
+
 
                 user_login_3(user_name=user_name,a1=a1, b1=b1, c1=c1,
                              d1=d1, e1=e1, a2=a2, b2=b2, c2=c2, d2=d2,
@@ -326,11 +312,19 @@ def main():
 
             def reset(self):
 
+                def turn_button(x=0):
 
-                # self.txtfld1.delete(0, len(self.txtfld1.get()))
-                # self.txtfld1.insert(0, "")
+                    self.txtfld1.destroy()
+                    self.txtfld1 = DateEntry(self.windows, font=("Helvetica", 10), state='readonly',
+                                             date_pattern='y-mm-dd', anchor='center')
+                    self.txtfld1.place(x=270, y=160, width=260)
 
+
+                self.txtfld1.destroy()
+                self.txtfld1 = ttk.Combobox(self.windows,font=("Helvetica", 10), state='readonly')
+                self.txtfld1.place(x=270, y=160, width=260)
                 self.txtfld1.set("")
+                self.txtfld1.bind("<Button-1>", turn_button)
 
 
                 self.txtfld2.delete(0, len(self.txtfld2.get()))
@@ -355,15 +349,14 @@ def main():
         window_user_login_2.attributes('-alpha', 0.97)
 
         user_login_window = User_2(window_user_login_2)
-        window_user_login_2.iconbitmap(default='DATA/IMAGES/icons/favicon.ico')
-        window_user_login_2.title('Get Bulk Data ' + '2.0.0')
+        # window_user_login_2.iconbitmap(default='DATA/IMAGES/icons/favicon.ico')
+        window_user_login_2.title('Get Bulk Data ' + '2')
         window_user_login_2.geometry("600x450")
         window_user_login_2.mainloop()
 
 
 
 
-    # version = "3.0.0"
 
 
 
@@ -381,80 +374,20 @@ def main():
 
     def user_login_3(user_name=str(0),a1=str(0),b1=str(0),c1=str(0),d1=str(0),e1=str(0),a2=str(0),b2=str(0),c2=str(0),d2=str(0),e2=str(0)):
 
-        #print(4)
+
 
 
         class User_3():
 
             def __init__(self, window):
 
-                # class CustomDateEntry(DateEntry):
-                #
-                #     def _select(self, event=None):
-                #         date = self._calendar.selection_get()
-                #         if date is not None:
-                #             self._set_text(date.strftime('%Y-%m-%d'))
-                #             self.event_generate('<<DateEntrySelected>>')
-                #         self._top_cal.withdraw()
-                #         if 'readonly' not in self.state():
-                #             self.focus_set()
+                self.windows=window
 
                 with open('DATA/Scanning/data.txt', 'r') as fh:
                     all_lines = fh.readlines()
                     for each in all_lines:
                         string = str(each)
 
-                #print(string)
-
-                # date_x = re.findall('17[0-9]{6}', string)
-                # try:
-                #     date_x = date_x[0]
-                # except:
-                #     date_x = ''
-                # date_x = '20' + date_x[2:4] + '-' + date_x[4:6] + '-' + date_x[6:8]
-                #
-                # gstin = re.findall('01[0-9]{14}', string)
-                # try:
-                #     gstin = gstin[0][2:]
-                # except:
-                #     gstin = ''
-                #
-                # lot = re.findall(r'10[0-9A-Za-z]*', string)
-                # try:
-                #     lot = lot[0][3:-1]
-                # except:
-                #     lot = ''
-                #
-                # serial = re.findall(r'21[0-9]*', string)
-                # try:
-                #     serial = serial[0][2:-1]
-                # except:
-                #     serial = ''
-
-                # strings=string.split("")
-                #
-                # for string in strings:
-                #
-                #     print(string)
-                #
-                #     x1=string.find('17')
-                #     if x1!=-1 and date_x=='':
-                #         date_x=string[x1+2:x1+8]
-                #         date_x='20'+date_x[0:2]+'-'+date_x[2:4]+'-'+date_x[:]
-                #
-                #     # x2 = string.find('01')
-                #     # if x2!=-1:
-                #     #     gstin = string[x2 + 2:x2 + 16]
-                #
-                #     x3 = string.find('21')
-                #     if x3!=-1 and serial=='':
-                #         serial = string[x3 + 2:]
-                #
-                #     x4 = string.find('10')
-                #     if x4 != -1 and lot=='':
-                #         lot = string[x4 + 2:]
-                #
-                #     print(date_x,gstin,lot,serial)
 
                 if a2 == str(0):
                     date_x = re.findall('17[0-9]{6}', string)
@@ -500,7 +433,7 @@ def main():
                 else:
                     serial = e2
 
-                #print(date_x, gstin, lot, serial)
+
 
                 def turn_button(x=0):
 
@@ -526,7 +459,7 @@ def main():
                 self.lb1 = tk.Label(window, text="EXP(YYYY-MM-DD)", font=("Helvetica", 10), bg='#EFEFEF')
                 self.lb1.place(x=60, y=160)
 
-                # self.txtfld1 = DateEntry(window,font=("Helvetica", 10),state='readonly',date_pattern='y-mm-dd',anchor='center')
+
                 self.txtfld1 = ttk.Combobox(window,
                                             font=("Helvetica", 10), state='readonly')
                 self.txtfld1.place(x=270, y=160, width=260)
@@ -550,19 +483,15 @@ def main():
                 self.txtfld3.insert(0, gstin)
 
                 self.lb4 = tk.Label(window, text="Total Bottles", font=("Helvetica", 10), bg='#EFEFEF')
-                #self.lb4.place(x=60, y=310)
+
 
                 self.txtfld4 = ttk.Entry(window,
                                          font=("Helvetica", 10))
-                #self.txtfld4.place(x=270, y=310, width=260)
                 self.txtfld4.insert(0, d1)
 
                 self.lb5 = tk.Label(window, text="Batch Size", font=("Helvetica", 10), bg='#EFEFEF')
-                #self.lb5.place(x=60, y=330)
-
                 self.txtfld5 = ttk.Entry(window,
                                          font=("Helvetica", 10))
-                #self.txtfld5.place(x=270, y=330, width=260)
                 self.txtfld5.insert(0, serial)
 
                 self.btn_back = ttk.Button(window, text="BACK", width=20, command=self.back)
@@ -687,7 +616,6 @@ def main():
 
                 window_user_login_3.destroy()
 
-                # IF VALIDATION IS SUCCESFULL THEN IT OPENS USER EDIT WINDOW
 
                 quiter(user_name=user_name,a1=a1, b1=b1, c1=c1,
                              d1=d1, e1=e1, a2=a2, b2=b2, c2=c2, d2=d2,
@@ -696,10 +624,20 @@ def main():
 
             def reset(self):
 
-                # self.txtfld1.delete(0, len(self.txtfld1.get()))
-                # self.txtfld1.insert(0, "")
+                def turn_button(x=0):
 
+                    self.txtfld1.destroy()
+                    self.txtfld1 = DateEntry(self.windows, font=("Helvetica", 10), state='readonly',
+                                             date_pattern='y-mm-dd', anchor='center')
+                    self.txtfld1.place(x=270, y=160, width=260)
+
+
+                self.txtfld1.destroy()
+                self.txtfld1 = ttk.Combobox(self.windows,font=("Helvetica", 10), state='readonly')
+                self.txtfld1.place(x=270, y=160, width=260)
                 self.txtfld1.set("")
+                self.txtfld1.bind("<Button-1>", turn_button)
+
 
                 self.txtfld2.delete(0, len(self.txtfld2.get()))
                 self.txtfld2.insert(0, "")
@@ -723,7 +661,7 @@ def main():
 
         user_login_window = User_3(window_user_login_3)
         window_user_login_3.iconbitmap(default='DATA/IMAGES/icons/favicon.ico')
-        window_user_login_3.title('Get Repacking Data ' + '3.0.0')
+        #window_user_login_3.title('Get Repacking Data ' + '3.0.0')
         window_user_login_3.geometry("600x450")
         window_user_login_3.mainloop()
 
